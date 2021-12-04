7825c28682d0a015498d2d9e4605ae592b772b08
Denis Petrov-Ploskirev <todenis@inbox.ru>
2021-12-04 14:51:19 +0300

    refactoring and jsdoc

--- js/index.js
+++ js/index.js
@@ -65,6 +65,11 @@ class App {
   static #hasInstance = false
   static #instance = null
 
+  /**
+   * 
+   * @param {Object} config - объект конфигурации, имеет поля data - исходные данные, selectors - селекторы необходимых элементов
+   * @returns {Object} - инстанс приложения (только один (синглтон))
+   */
   constructor(config) {
     if (App.#hasInstance) {
       console.warn('Instance is already exist. Returning existing instance')
@@ -86,41 +91,68 @@ class App {
     App.#hasInstance = true
   }
 
+  /**
+   * Метод для формирования массива с айдишниками контактов, 
+   * которые не являются друзьями конкретного контакта.
+   * Если список уже был получен, вернет существующий список
+   * @param {Object} person - объект контакта из списка контактов
+   * @param {Number} quantity - Количество элементов возвращаемого массива 
+   * @returns {Array} - массив самых айдишников контактов, которых нет в друзьях
+   */
   getNotFriendsList(person, quantity = 3) {
+    if (person.notFriends) {
+      return person.notFriends
+    }
     const notFriendsList = []
     for (let i = 0; notFriendsList.length < quantity; i++) {
-      if (!person.friends.includes(this.allIds[i])) {
+      if (!person.friends.includes(this.allIds[i]) && (this.allIds[i] !== person.id)) {
         notFriendsList.push(this.allIds[i])
       }
     }
     return notFriendsList
   }
 
+  /**
+   * Метод для преобразования исходного массива в объект с ключами - id контактов
+   * Также подсчитывает индекс популярности для каждого контакта 
+   * и формирует список "недрузей" вызывая для каждого this.getNotFriendsList
+   * Далее используется именно этот список для снижения сложности
+   * Если уже есть вычисленная ранее структура, вернет ее
+   * @param {Array} data - Массив исходных данных (список контактов)
+   * @returns {Object} - Преобразованный список контактов, приведенный к объекту
+   */
   getMappedData(data) {
+    if (this.mappedData) {
+      return this.mappedData
+    }
+
     const res = {}
-  
     data.forEach(person => {
       const notFriends = this.getNotFriendsList(person, 3)
-      
-      if (res[person.id]) {
-        res[person.id] = {...person, ...res[person.id], notFriends}
-       } else {
-         res[person.id] = {...person, notFriends, popularity: 0}
-       }
+
+      res[person.id] = res[person.id] ? 
+        {...person, ...res[person.id], notFriends} : 
+        {...person, notFriends, popularity: 0}
       
       person.friends.forEach(friendId => {
-        if (res[friendId]) {
-          res[friendId] = {...res[friendId], popularity: ++res[friendId].popularity}
-        } else {
-          res[friendId] = {popularity: 1}
-        }
+        res[friendId] = res[friendId] ? 
+          {...res[friendId], popularity: ++res[friendId].popularity} : 
+          {popularity: 1}
       })
-      
     })
     return res
   }
 
+  /**
+   * Метод для получения списка популярных людей
+   * Если ранее список уже был вычислен, вернет вычисленный список
+   * @param {Number} quantity - Количество элементов возвращаемого массива 
+   * @returns {Array} - массив самых популярных людей, отсортированный по популярности и именам
+   */
   getPopularPeopleList(quantity = 3) {
+    if (this.popularPeople) {
+      return this.popularPeople
+    }
     return Object.values(this.mappedData).sort((a, b) => {
       if (a.popularity === b.popularity) {
         if (a.name < b.name) {
@@ -136,6 +168,11 @@ class App {
     }).slice(0, quantity)
   }
 
+  /**
+   * Метод возвращает разметку для элемента списка контактов
+   * @param {Object} person - объект контакта из списка контактов
+   * @returns {String} - строка с разметкой для элемента списка контактов
+   */
   renderListItem(person) {
     return `
       <li data-id="${person.id}">
@@ -144,14 +181,23 @@ class App {
     `
   }
 
+  /**
+   * Метод рендерит список контактов в список, полученный через селектор 'ContactsList'
+   * @returns {undefined}
+   */
   renderContactsList() {
     let listContent = ''
     Object.values(this.mappedData).forEach(person => {
       listContent += this.renderListItem(person)
     })
-    return listContent
+    this.contactsList.innerHTML = listContent
   }
 
+  /**
+   * Метод возвращает разметку для элемента details списка
+   * @param {Object} person - объект контакта из списка контактов
+   * @returns {String} - строка с разметкой для элемента списка details
+   */
   renderDetailsListItem(person) {
     return `
       <li>
@@ -161,12 +207,22 @@ class App {
     `
   }
 
+  /**
+   * Метод возвращает разметку для заголовка в списке details
+   * @param {String} title - Заголовок
+   * @returns {String} - строка с разметкой для заголовка в списке details
+   */
   renderDetailsListTitle(title) {
     return `
       <li class="people-title">${title}</li>
     `
   }
 
+  /**
+   * Метод рендера для отрисовки списка details конкретного элемента списка контактов
+   * @param {String} personId -  id контакта, для которого нужно отрисовать список details
+   * @returns {undefined}
+   */
   renderDetailsList(personId) {
     let listContent = ''
     const person = this.mappedData[personId]
@@ -176,31 +232,57 @@ class App {
     listContent += person.notFriends.reduce((acc, humanId) => acc + this.renderDetailsListItem(this.mappedData[humanId]),'')
     listContent += this.renderDetailsListTitle('Популярные люди')
     listContent += this.popularPeople.reduce((acc, human) => acc + this.renderDetailsListItem(human),'')
-    return listContent
+    this.detailsList.innerHTML = listContent
   }
 
+  /**
+   * Метод для отрисовки имени выбранного контакта
+   * @param {String} id - id контакта, для которого нужно отрисовать список details
+   * @returns {undefined}
+   */
+  renderName(id) {
+    const name = this.mappedData[id].name
+    this.nameWrapper.innerHTML = name
+  }
+
+   /**
+    * Метод для отрисовки и показа details списка конкретного контакта
+    * @param {Object} evt - объект события
+    */
   showDetails(evt) {
     const target = evt.target
     const id = target.dataset.id || target.closest('li').dataset.id
     const isListItem = Boolean(id)
     if (isListItem) {
       this.container.classList.add('details')
-      this.nameWrapper.innerHTML = this.mappedData[id].name
-      this.detailsList.innerHTML = this.renderDetailsList(id)
+      this.renderName(id)
+      this.renderDetailsList(id)
     }
   }
 
+  /**
+   * Метод для сокрытия details списка конкретного контакта
+   */
   hideDetails() {
     this.container.classList.remove('details')
   }
 
+  /**
+   * Метод для инициализации приложения
+   * Отрисовывает список контактов
+   * Вешает обработчики событий
+   */
   initApp() {
-    this.contactsList.innerHTML = this.renderContactsList(this.mappedData)
+    this.renderContactsList(this.mappedData)
     this.contactsList.addEventListener('click', this.showDetails.bind(this))
     this.backButton.addEventListener('click', this.hideDetails.bind(this))
+    setTimeout(() => {this.container.classList.remove('initial')}, 0)
   }
 }
 
+/**
+ * Стартуем приложение по готовности DOM
+ */
 window.addEventListener('DOMContentLoaded', () => {
   const config = {
     data,
